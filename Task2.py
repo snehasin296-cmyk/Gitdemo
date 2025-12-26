04bd8fe0b9e5fd99e246ca0b77d887ce8e5ec788
snehasin296-cmyk <snehasin296@gmail.com>
2025-12-26 14:24:35 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,5 @@
+first_name = input("Enter the first name: ")
+last_name = input("Enter the second name: ")
+
+full_name = first_name + " " + last_name
+print("Hello, " + full_name + "! Welcome to the python program")
